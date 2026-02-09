7484fb22fa29df776195b6985583f85d09d5b56a
Li Mace <lygmace@gmail.com>
2026-02-09 15:20:23 -0600

    o3-navigation-jsx exercise

--- src/01-introduction/03-navigating-jsx-types.problem.tsx
+++ src/01-introduction/03-navigating-jsx-types.problem.tsx
@@ -2,11 +2,15 @@ export const Component = () => {
   return (
     <div
       // How do I figure out what type aria-posinset expects?
-      aria-posinset={}
+      // hover over aria-posinset to see the error message.
+      aria-posinset={1}
       // How do I figure out what type onChange expects?
-      onChange={}
+      onChange={()=>console.log("clicked")}
 
       // How do I get autocomplete with JSX?
+      // control + space
     />
   );
 };
+
+// type Example = React.FormEventHandler<HTMLDivElement> | undefined
