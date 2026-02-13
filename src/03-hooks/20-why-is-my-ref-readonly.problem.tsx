e78223a08d58778ceabe8b504392e9d831091c6e
Li Mace <lygmace@gmail.com>
2026-02-13 09:13:00 -0600

    hooks type

--- src/03-hooks/20-why-is-my-ref-readonly.problem.tsx
+++ src/03-hooks/20-why-is-my-ref-readonly.problem.tsx
@@ -1,6 +1,10 @@
 import { useRef } from "react";
 
 export const Component = () => {
+
+
+  // passing null here means you want React to manage this ref for you. 
+  // to fix it, we can just delete the null
   const ref = useRef<string>(null);
 
   // Why is this not allowed?
